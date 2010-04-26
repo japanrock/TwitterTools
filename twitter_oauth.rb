8b55501d294334f9ea648d7842accb77ccb471b4
福重 伸太朗 <japanrock@fukushige-shintarou-no-iMac.local>
2010-04-26 11:09:04 +0900

    twitterのoauth認証からポストなどをサポートする

---
+++ twitter_oauth.rb
@@ -0,0 +1,47 @@
+# TwitterのAPIとのやりとりを行うクラス
+class TwitterOauth
+  def initialize
+    # カレントディレクトリのsercret_keys.ymlをloadします。
+    @secret_keys = YAML.load_file(File.dirname(__FILE__) + '/sercret_key.yml')
+  end
+  
+  def consumer_key
+    @secret_keys["ConsumerKey"]
+  end
+
+  def consumer_secret
+    @secret_keys["ConsumerSecret"]
+  end
+
+  def access_token_key
+    @secret_keys["AccessToken"]
+  end
+
+  def access_token_secret
+    @secret_keys["AccessTokenSecret"]
+  end
+
+  def consumer
+    @consumer = OAuth::Consumer.new(
+      consumer_key,
+      consumer_secret,
+      :site => 'http://twitter.com'
+    )
+  end
+
+  def access_token
+    consumer
+    access_token = OAuth::AccessToken.new(
+      @consumer,
+      access_token_key,
+      access_token_secret
+    )
+  end
+
+  def post(tweet=nil)
+    response = access_token.post(
+      'http://twitter.com/statuses/update.json',
+      'status'=> tweet
+    )
+  end
+end
